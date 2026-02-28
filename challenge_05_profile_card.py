f74d5b4151f94c84a65ec0621efbac87791be927
kavyapc <36132470+kavyapc@users.noreply.github.com>
2026-02-28 16:56:47 -0700

    Update challenge_05_profile_card.py

--- challenge_05_profile_card.py
+++ challenge_05_profile_card.py
@@ -1,9 +1,7 @@
-
 name= str(input("Enter your name:"))
 age =int(input("Enter your age:"))
 height =float(input("Enter your height in metres:"))
 height1 = int(height*100)
-
 
 number = int (input("Enter your favourite number:"))
 
@@ -17,5 +15,6 @@ print(f"Name:{name} ")
 print(f"Age:{age} ") 
 print (f"Height:{height}m  ({height1}cm)")  
 print  (f"Favourite Number :{number}")
+
 
 print("-------------------------------------------")
