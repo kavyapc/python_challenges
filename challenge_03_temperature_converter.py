22910545bd5058adb89552def1b2e5f6874bf9e4
kavyapc <36132470+kavyapc@users.noreply.github.com>
2026-02-28 17:34:55 -0700

    Update challenge_03_temperature_converter.py

--- challenge_03_temperature_converter.py
+++ challenge_03_temperature_converter.py
@@ -1,3 +1,4 @@
 celsius = float(input("Enter the temperature in celsius:"))
 fahrenheit = (celsius * 9/5) + 32
-print(f" {celsius} is equal to {fahrenheit:.2f}")
+print(f" {celsius}\u00b0C is equal to {fahrenheit:.2f}\u00b0F")
+
